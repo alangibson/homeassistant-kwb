6a361348c61479f02ce69c415de65c49c5aab0ef
alangibson <alan.gibson@gmail.com>
2023-12-26 23:58:09 +0100

    Temp disabling update handler

--- custom_components/kwb_heaters/__init__.py
+++ custom_components/kwb_heaters/__init__.py
@@ -20,12 +20,12 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
 
     hass_data = dict(config_entry.data)
 
-    # Register options flow
+    # Register options update handler
     # Registers update listener to update config entry when options are updated.
-    unsub_options_update_listener = config_entry.add_update_listener(options_update_listener)
+    # unsub_options_update_listener = config_entry.add_update_listener(options_update_listener)
     # Store a reference to the unsubscribe function to cleanup if an entry is unloaded.
-    hass_data["unsub_options_update_listener"] = unsub_options_update_listener
-    hass.data[DOMAIN][config_entry.entry_id] = hass_data
+    # hass_data["unsub_options_update_listener"] = unsub_options_update_listener
+    # hass.data[DOMAIN][config_entry.entry_id] = hass_data
 
     # Forward the setup to the sensor platform.
     hass.async_create_task(
